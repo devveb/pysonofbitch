3b36a9092b33024e7c3151b6ea650b02a2ef0c77
thkim <thkim@mail.me>
2026-04-02 13:02:02 +0900

    deploy

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {RouterSetup} from "./RouterSetup";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterSetup  />
-    </React.StrictMode>
-);
